69e1d37d364cfcbdbba2a031972b34674d0e58f8
User <javlonbekgit10@gmail.com>
2025-08-13 23:43:19 -0700

    fix_mistakes

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -51,12 +51,18 @@ class AuthRepository {
   }
 
   Future<bool> forgotPassword(String email) async {
-    var result = await client.forgotPassword(email);
+    var result = await client.genericPostRequestV2(
+      "/auth/reset-password/email",
+      data: {"email": email},
+    );
     return result;
   }
 
   Future<bool> enterCode(String email, String code) async {
-    var result = await client.enterCode(email, code);
+    var result = await client.genericPostRequestV2(
+      '/auth/reset-password/verify',
+      data: {"email": email, "code": code},
+    );
     return result;
   }
 
@@ -65,7 +71,10 @@ class AuthRepository {
     required String code,
     required String password,
   }) async {
-    var result = await client.resetPassword(email, code, password);
+    var result = await client.genericPostRequestV2(
+      '/auth/reset-password/reset',
+      data: {"email": email, "code": code, "password": password},
+    );
     return result;
   }
 }
